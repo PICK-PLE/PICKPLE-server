60aaf2cc2272d5023a7fbabbc350ba541475b652
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-07-04 20:24:57 +0900

    feat : ResponseHeader의 http status code를 에러 코드와 일치되도록 구현

---
+++ src/main/java/com/pickple/server/global/exception/ResponseStatusSetterAdvice.java
@@ -0,0 +1,35 @@
+package com.pickple.server.global.exception;
+
+import com.pickple.server.global.response.ApiResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+
+@RestControllerAdvice
+public class ResponseStatusSetterAdvice implements ResponseBodyAdvice<ApiResponse<?>> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return returnType.getParameterType() == ApiResponse.class;
+    }
+
+    @Override
+    public ApiResponse<?> beforeBodyWrite(
+            ApiResponse body,
+            MethodParameter returnType,
+            MediaType selectedContentType,
+            Class selectedConverterType,
+            ServerHttpRequest request,
+            ServerHttpResponse response
+    ) {
+        HttpStatus status = body.httpStatus();
+        response.setStatusCode(status);
+
+        return body;
+    }
+}
