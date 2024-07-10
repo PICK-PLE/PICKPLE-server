a41140049d615a46f09f876d84cfeb369109eb75
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-07-10 18:47:22 +0900

    hotfix : cors 에러해결

--- src/main/java/com/pickple/server/global/config/WebConfig.java
+++ src/main/java/com/pickple/server/global/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.pickple.server.global.config;
 
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
@@ -10,11 +12,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
+        CorsConfiguration config = new CorsConfiguration();
+
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:5173",
                         "https://pick-ple.com:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .maxAge(3000);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
     }
 }
