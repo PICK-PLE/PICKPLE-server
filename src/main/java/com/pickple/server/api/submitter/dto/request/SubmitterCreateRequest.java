5b0cd947ff25bd37dcf8eb8546c5d35069db48d0
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-07-11 08:25:42 +0900

    feat : dto 구현

---
+++ src/main/java/com/pickple/server/api/submitter/dto/request/SubmitterCreateRequest.java
@@ -0,0 +1,32 @@
+package com.pickple.server.api.submitter.dto.request;
+
+import com.pickple.server.api.submitter.domain.SubmitterCategoryInfo;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record SubmitterCreateRequest(
+        @Size(max = 300, message = "소개글은 최대 300자 이내로 작성해주세요.")
+        @NotBlank(message = "소개글이 비어 있습니다.")
+        String intro,      //	관리자에게 보여질 intro
+
+        @Size(max = 300, message = "목표는 최대 300자 이내로 작성해주세요.")
+        @NotBlank(message = "목표가 비어 있습니다.")
+        String goal,        //이루고 싶은 목표
+
+        @NotBlank(message = "링크가 비어 있습니다.")
+        String link,    //호스트의 sns 및 홈페이지 링크
+
+        @Size(max = 10, message = "닉네임은 최대 15자 이내로 작성해주세요.")
+        @NotBlank(message = "닉네임이 비어 있습니다.")
+        String nickname,    //호스트 승인 후 사용될 닉네임
+
+        SubmitterCategoryInfo categoryList,    //호스트 카테고리 리스트
+
+        @Size(max = 300, message = "글모임명은 최대 300자 이내로 작성해주세요.")
+        @NotBlank(message = "계획이 비어 있습니다.")
+        String plan,    //클래스 모임 운영 계획
+
+        @NotBlank(message = "메일이 비어 있습니다.")
+        String email    //호스트 승인 후 연락받을 메일 주소
+) {
+}
