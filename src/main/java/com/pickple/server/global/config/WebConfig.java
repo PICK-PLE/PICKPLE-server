d41971fac97b6657940f4a3eb7f176b36513b5f2
boram <128011308+bo-ram-bo-ram@users.noreply.github.com>
2024-07-10 18:06:20 +0900

    hotfix : EnableWebMvc import추가

--- src/main/java/com/pickple/server/global/config/WebConfig.java
+++ src/main/java/com/pickple/server/global/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.pickple.server.global.config;
 
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
