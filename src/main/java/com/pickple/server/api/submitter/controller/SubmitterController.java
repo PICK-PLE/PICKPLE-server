539cd9b81123af187625092c97f64c879fe00bcd
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-07-11 08:23:39 +0900

    feat : controller구현

---
+++ src/main/java/com/pickple/server/api/submitter/controller/SubmitterController.java
@@ -0,0 +1,27 @@
+package com.pickple.server.api.submitter.controller;
+
+import com.pickple.server.api.submitter.dto.request.SubmitterCreateRequest;
+import com.pickple.server.api.submitter.service.SubmitterCommandService;
+import com.pickple.server.global.common.annotation.UserId;
+import com.pickple.server.global.response.ApiResponseDto;
+import com.pickple.server.global.response.enums.SuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class SubmitterController {
+
+    private final SubmitterCommandService submitterCommandService;
+
+    @PostMapping("/v1/submitter")
+    public ApiResponseDto postSubmitter(@UserId final Long userId,
+                                        @RequestBody SubmitterCreateRequest submitterCreateRequest) {
+        submitterCommandService.createSubmitter(userId, submitterCreateRequest);
+        return ApiResponseDto.success(SuccessCode.SUBMITTER_POST_SUCCESS);
+    }
+}
