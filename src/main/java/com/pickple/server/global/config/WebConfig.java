e9ba5f4ff9c2d8fcc5d3dee49961ff0b2098f31e
boram <128011308+bo-ram-bo-ram@users.noreply.github.com>
2024-07-10 17:59:28 +0900

    hotfix : cors 에러 해결

--- src/main/java/com/pickple/server/global/config/WebConfig.java
+++ src/main/java/com/pickple/server/global/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
