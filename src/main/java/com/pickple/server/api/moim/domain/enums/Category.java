25a9857a999b3c8d3b501804ec5e3035b03692d4
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-07-10 23:40:31 +0900

    fix : String 한글로 수정 및 getCategories 메서드 추가

--- src/main/java/com/pickple/server/api/moim/domain/enums/Category.java
+++ src/main/java/com/pickple/server/api/moim/domain/enums/Category.java
@@ -1,20 +1,30 @@
 package com.pickple.server.api.moim.domain.enums;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum Category {
 
-    NJOB("njob"),
-    INVESTMENT("investment"),
-    STARTUP("startup"),
-    EMPLOYMENT("employment"),
-    PRODUCTIVITY("productivity"),
-    LIFESTYLE("lifestyle"),
-    HEALTH("health"),
-    MIND("mind"),
-    HOBBY("hobby"),
-    LANGUAGE("language");
+    NJOB("N잡"),
+    INVESTMENT("재테크"),
+    STARTUP("창업"),
+    EMPLOYMENT("취업,이직"),
+    PRODUCTIVITY("생산성"),
+    LIFESTYLE("라이프스타일"),
+    HEALTH("건강"),
+    MIND("마인드"),
+    HOBBY("취미"),
+    LANGUAGE("외국어");
     public final String category;
 
     Category(String category) {
         this.category = category;
+    }
+
+    public static List<String> getCategories() {
+        return Arrays.stream(Category.values())
+                .map(category -> category.category)
+                .collect(Collectors.toList());
     }
 }
