4b34cf7bce8d75a1e083cc60e3b5900740191fee
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-07-10 15:40:43 +0900

    chore : 기타 import 변경 사항 수정

--- src/main/java/com/pickple/server/api/guest/domain/Guest.java
+++ src/main/java/com/pickple/server/api/guest/domain/Guest.java
@@ -1,13 +1,25 @@
 package com.pickple.server.api.guest.domain;
 
-import com.pickple.server.api.applicant.domain.Submitter;
+import com.pickple.server.api.submitter.domain.Submitter;
 import com.pickple.server.api.user.domain.User;
 import com.pickple.server.global.common.domain.BaseTimeEntity;
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
@@ -37,7 +49,7 @@ public class Guest extends BaseTimeEntity {
             final User user,
             final String nickname,
             final String imageUrl
-    ){
+    ) {
         this.user = user;
         this.nickname = nickname;
         this.imageUrl = imageUrl;
