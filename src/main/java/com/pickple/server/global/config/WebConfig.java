09e671e505c861d7309e42aea5f504803033a7ae
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-07-11 05:35:34 +0900

    feat : custom 어노테이션 (@UserId : userId 반환) 구현

--- src/main/java/com/pickple/server/global/config/WebConfig.java
+++ src/main/java/com/pickple/server/global/config/WebConfig.java
@@ -1,14 +1,23 @@
 package com.pickple.server.global.config;
 
+import com.pickple.server.global.common.annotation.UserIdResolver;
 import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@RequiredArgsConstructor
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    private final UserIdResolver userIdResolver;
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -22,5 +31,10 @@ public class WebConfig {
         config.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userIdResolver);
     }
 }
