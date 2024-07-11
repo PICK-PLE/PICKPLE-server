9bc3906424ee84cb71b96945b45e6a4cdedb818b
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-07-11 16:04:25 +0900

    feat : JSON 반환을 위한 Info 객체 생성

---
+++ src/main/java/com/pickple/server/api/moim/domain/CategoryInfo.java
@@ -0,0 +1,11 @@
+package com.pickple.server.api.moim.domain;
+
+import lombok.Getter;
+
+@Getter
+public class CategoryInfo {
+
+    private String category1;
+    private String category2;
+    private String category3;
+}
---
+++ src/main/java/com/pickple/server/api/moim/domain/ImageInfo.java
@@ -0,0 +1,10 @@
+package com.pickple.server.api.moim.domain;
+
+import lombok.Getter;
+
+@Getter
+public class ImageInfo {
+    private String imageUrl1;
+    private String imageUrl2;
+    private String imageUrl3;
+}
