79aa39c207899e473294f9553edbea277a04b19b
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-07-11 23:24:14 +0900

    feat : moim 유효성 검증 로직 추가

--- src/main/java/com/pickple/server/api/moim/repository/MoimRepository.java
+++ src/main/java/com/pickple/server/api/moim/repository/MoimRepository.java
@@ -1,8 +1,16 @@
 package com.pickple.server.api.moim.repository;
 
 import com.pickple.server.api.moim.domain.Moim;
+import com.pickple.server.global.exception.CustomException;
+import com.pickple.server.global.response.enums.ErrorCode;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MoimRepository extends JpaRepository<Moim, Long> {
+    Optional<Moim> findMoimById(Long id);
 
+    default Moim findMoimByIdOrThrow(Long id) {
+        return findMoimById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.MOIM_NOT_FOUND));
+    }
 }
