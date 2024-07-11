137de56803b7b1f79e8a4de8e12c8f15cb85fd94
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-07-12 05:15:29 +0900

    feat : controller 구현

--- src/main/java/com/pickple/server/api/moim/controller/MoimController.java
+++ src/main/java/com/pickple/server/api/moim/controller/MoimController.java
@@ -1,15 +1,20 @@
 package com.pickple.server.api.moim.controller;
 
 import com.pickple.server.api.moim.domain.enums.Category;
+import com.pickple.server.api.moim.dto.request.MoimCreateRequest;
 import com.pickple.server.api.moim.dto.response.MoimDetailResponse;
 import com.pickple.server.api.moim.dto.response.SubmittedMoimResponse;
+import com.pickple.server.api.moim.service.MoimCommandService;
 import com.pickple.server.api.moim.service.MoimQueryService;
+import com.pickple.server.global.common.annotation.HostId;
 import com.pickple.server.global.response.ApiResponseDto;
 import com.pickple.server.global.response.enums.SuccessCode;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +24,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class MoimController {
 
     private final MoimQueryService moimQueryService;
+    private final MoimCommandService moimCommandService;
+
+    @PostMapping("/v1/moim")
+    public ApiResponseDto createMoim(@HostId Long hostId, @RequestBody MoimCreateRequest moimCreateRequest) {
+        moimCommandService.createMoim(hostId, moimCreateRequest);
+        return ApiResponseDto.success(SuccessCode.MOIM_CREATE_SUCCESS);
+    }
 
     @GetMapping("/v1/moim/categories")
     public ApiResponseDto<List<String>> getAllCategories() {
