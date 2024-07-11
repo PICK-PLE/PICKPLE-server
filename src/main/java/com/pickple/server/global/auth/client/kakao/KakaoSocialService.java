1e3f29d2acec72ea5b50e48d29342891d5498c81
boram <128011308+bo-ram-bo-ram@users.noreply.github.com>
2024-07-12 05:03:46 +0900

    hotfix : kakao redirect url 클라쌤 로컬 환경으로 수정

--- src/main/java/com/pickple/server/global/auth/client/kakao/KakaoSocialService.java
+++ src/main/java/com/pickple/server/global/auth/client/kakao/KakaoSocialService.java
@@ -21,7 +21,7 @@ import org.springframework.stereotype.Service;
 public class KakaoSocialService implements SocialService {
 
     private static final String AUTH_CODE = "authorization_code";
-    private static final String REDIRECT_URI = "http://localhost:8080/kakao/callback";
+    private static final String REDIRECT_URI = "http://localhost:5173/kakao/redirection";
 
     @Value("${kakao.clientId}")
     private String clientId;
