ae7b46623d60ddb1fff45df17101e919f08ab691
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-07-12 03:38:24 +0900

    refactor : 객체 생성 빌더 패턴으로 리팩토링

--- src/main/java/com/pickple/server/api/moim/dto/response/MoimDetailResponse.java
+++ src/main/java/com/pickple/server/api/moim/dto/response/MoimDetailResponse.java
@@ -16,42 +16,4 @@ public record MoimDetailResponse(
         ImageInfo imageList,
         Long hostId
 ) {
-    public record DateList(
-            String date,
-            String dayOfWeek,
-            String startTime,
-            String endTime
-    ) {
-    }
-
-    public record ImageList(
-            String imageUrl1,
-            String imageUrl2,
-            String imageUrl3
-    ) {
-    }
-
-    public static MoimDetailResponse of(
-            int dayOfDay,
-            String title,
-            DateInfo dateList,
-            boolean isOffline,
-            String spot,
-            int maxGuest,
-            int fee,
-            ImageInfo imageList,
-            Long hostId
-    ) {
-        return MoimDetailResponse.builder()
-                .dayOfDay(dayOfDay)
-                .title(title)
-                .dateList(dateList)
-                .isOffline(isOffline)
-                .spot(spot)
-                .maxGuest(maxGuest)
-                .fee(fee)
-                .imageList(imageList)
-                .hostId(hostId)
-                .build();
-    }
 }
