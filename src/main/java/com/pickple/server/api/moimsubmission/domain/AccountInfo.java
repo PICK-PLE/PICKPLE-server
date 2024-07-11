9e4809802ca6bf7f795e2cb24eb287bb3bfa39f4
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-07-11 23:23:05 +0900

    feat : JSON 입력을 위한 Info 객체 생성

---
+++ src/main/java/com/pickple/server/api/moimsubmission/domain/AccountInfo.java
@@ -0,0 +1,13 @@
+package com.pickple.server.api.moimsubmission.domain;
+
+import lombok.Getter;
+
+@Getter
+public class AccountInfo {
+
+    private String holder;
+
+    private String bank;
+
+    private String accountNumber;
+}
