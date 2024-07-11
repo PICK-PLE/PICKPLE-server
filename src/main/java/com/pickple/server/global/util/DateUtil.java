94cdaa2a696eafbbe09eca8f759bf4ac0cea4944
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-07-11 16:04:46 +0900

    feat : Date 관련 처리를 위한 DateUtil 클래스 구현

---
+++ src/main/java/com/pickple/server/global/util/DateUtil.java
@@ -0,0 +1,13 @@
+package com.pickple.server.global.util;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class DateUtil {
+
+    public static int calculateDayOfDay(LocalDate date) {
+        LocalDate today = LocalDate.now();
+        int dday = (int) ChronoUnit.DAYS.between(today, date);
+        return dday;
+    }
+}
