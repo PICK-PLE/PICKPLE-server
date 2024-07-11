5e8fd74367b9879653ebf45a2e0945f5d19a8bb8
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-07-11 16:03:37 +0900

    feat : Controller 구현

--- src/main/java/com/pickple/server/api/moim/controller/MoimController.java
+++ src/main/java/com/pickple/server/api/moim/controller/MoimController.java
@@ -1,12 +1,14 @@
 package com.pickple.server.api.moim.controller;
 
 import com.pickple.server.api.moim.domain.enums.Category;
-import com.pickple.server.api.moim.service.MoimService;
+import com.pickple.server.api.moim.dto.response.MoimDetailResponse;
+import com.pickple.server.api.moim.service.MoimCommandService;
 import com.pickple.server.global.response.ApiResponseDto;
 import com.pickple.server.global.response.enums.SuccessCode;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MoimController {
 
-    private final MoimService moimService;
+    private final MoimCommandService moimCommandService;
 
     @GetMapping("/v1/moim/categories")
     public ApiResponseDto<List<String>> getAllCategories() {
         return ApiResponseDto.success(SuccessCode.ALL_CATEGORY_GET_SUCCESS, Category.getCategories());
+    }
+
+    @GetMapping("/v1/moim/{moimId}")
+    public ApiResponseDto<MoimDetailResponse> getMoimDetail(@PathVariable Long moimId) {
+        return ApiResponseDto.success(SuccessCode.MOIM_DETAIL_GET_SUCCESS, moimCommandService.getMoimDetail(moimId));
     }
 }
