b268bd306a2fc288fc2c0592b50f8da3bdcb887d
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-07-12 00:18:27 +0900

    fix : 누락된 컬럼 추가

--- src/main/java/com/pickple/server/api/host/domain/Host.java
+++ src/main/java/com/pickple/server/api/host/domain/Host.java
@@ -3,11 +3,25 @@ package com.pickple.server.api.host.domain;
 import com.pickple.server.api.moim.domain.Moim;
 import com.pickple.server.api.user.domain.User;
 import com.pickple.server.global.common.domain.BaseTimeEntity;
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 @Entity
 @Getter
@@ -29,17 +43,11 @@ public class Host extends BaseTimeEntity {
 
     private String imageUrl;
 
+    @JdbcTypeCode(SqlTypes.JSON)
+    private HostCategoryInfo categoryList;
+
+    private String link;
+
     @OneToMany(mappedBy = "host", cascade = CascadeType.REMOVE)
     private List<Moim> moims = new ArrayList<>();
-
-    @Builder
-    public Host(
-            final User user,
-            final String nickname,
-            final String imageUrl
-    ){
-        this.user = user;
-        this.nickname = nickname;
-        this.imageUrl = imageUrl;
-    }
 }
