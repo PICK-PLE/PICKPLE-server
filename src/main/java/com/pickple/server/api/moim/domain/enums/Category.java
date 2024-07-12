88b1fc962fd222021c195952d248918c3d10bb02
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-07-13 04:33:55 +0900

    chore : category enum 관련 수정

--- src/main/java/com/pickple/server/api/moim/domain/enums/Category.java
+++ src/main/java/com/pickple/server/api/moim/domain/enums/Category.java
@@ -3,7 +3,11 @@ package com.pickple.server.api.moim.domain.enums;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
 public enum Category {
 
     NJOB("njob"),
@@ -16,16 +20,12 @@ public enum Category {
     MIND("mind"),
     HOBBY("hobby"),
     LANGUAGE("language");
-    public final String category;
 
-    Category(String category) {
-        this.category = category;
-    }
+    public final String category;
 
     public static List<String> getCategories() {
         return Arrays.stream(Category.values())
                 .map(category -> category.category)
                 .collect(Collectors.toList());
     }
-
 }
--- src/main/java/com/pickple/server/api/submitter/domain/SubmitterCategoryInfo.java
+++ src/main/java/com/pickple/server/api/submitter/domain/SubmitterCategoryInfo.java
@@ -12,14 +12,8 @@ public class SubmitterCategoryInfo {
 
     @NotBlank(message = "카테고리가 비었습니다.")
     private final String category1;
-    private final String category2;
-    private final String category3;
 
-    public static SubmitterCategoryInfo of(String category1, String category2, String category3) {
-        return SubmitterCategoryInfo.builder()
-                .category1(category1)
-                .category2(category2)
-                .category3(category3)
-                .build();
-    }
+    private final String category2;
+
+    private final String category3;
 }
