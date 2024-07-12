8ef071665807cce5388dc5338377fd7ecb7ff5ca
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-07-12 19:27:57 +0900

    feat : 생성일시 포맷을 위한 util 구현

---
+++ src/main/java/com/pickple/server/api/notice/dto/response/NoticeListGetByMoimResponse.java
@@ -0,0 +1,16 @@
+package com.pickple.server.api.notice.dto.response;
+
+import lombok.Builder;
+
+@Builder
+public record NoticeListGetByMoimResponse(
+        Long noticeId,   //공지사항id
+        String hostNickName,   //호스트 닉네임
+        String hostImageUrl,   //	호스트 이미지
+        String title,    //공지사항 제목
+        String content,    //공지사항 내용
+        String date,    //공지사항 등록 일자 및 시간 yyyy.mm.dd hh:mm:ss
+        String noticeImageUrl, //공지사항 이미지
+        Long hostId//	호스트 id
+) {
+}
