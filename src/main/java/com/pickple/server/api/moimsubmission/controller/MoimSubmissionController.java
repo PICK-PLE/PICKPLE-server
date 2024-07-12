a592b8d33ae20bb5a90c0a44409599c7801ba098
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-07-13 00:44:12 +0900

    feat : controller 구현

--- src/main/java/com/pickple/server/api/moimsubmission/controller/MoimSubmissionController.java
+++ src/main/java/com/pickple/server/api/moimsubmission/controller/MoimSubmissionController.java
@@ -1,15 +1,19 @@
 package com.pickple.server.api.moimsubmission.controller;
 
+import com.pickple.server.api.moimsubmission.domain.MoimSubmissionState;
 import com.pickple.server.api.moimsubmission.dto.request.MoimSubmitRequest;
 import com.pickple.server.api.moimsubmission.service.MoimSubmissionCommandService;
+import com.pickple.server.api.moimsubmission.service.MoimSubmissionQueryService;
 import com.pickple.server.global.common.annotation.GuestId;
 import com.pickple.server.global.response.ApiResponseDto;
 import com.pickple.server.global.response.enums.SuccessCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MoimSubmissionController {
 
     private final MoimSubmissionCommandService moimSubmissionCommandService;
+    private final MoimSubmissionQueryService moimSubmissionQueryService;
 
     @PostMapping("/v1/moim/{moimId}")
     public ApiResponseDto createMoimSubmission(
@@ -27,5 +32,14 @@ public class MoimSubmissionController {
     ) {
         moimSubmissionCommandService.createMoimSubmission(moimId, guestId, moimSubmitRequest);
         return ApiResponseDto.success(SuccessCode.MOIM_SUBMISSION_POST_SUCCESS);
+    }
+
+    @GetMapping("/v1/guest/{guestId}/submitted-moim-list")
+    public ApiResponseDto getSubmittedMoimListByGuest(
+            @PathVariable Long guestId,
+            @RequestParam MoimSubmissionState moimSubmissionState
+    ) {
+        return ApiResponseDto.success(SuccessCode.SUBMITTED_MOIM_LIST_BY_GUEST_GET_SUCCESS,
+                moimSubmissionQueryService.getSubmittedMoimListByGuest(guestId, moimSubmissionState));
     }
 }
