64606b07a8dc8a08ee12d0ca5fcd721824169764
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-07-13 00:31:59 +0900

    feat : 홈 배너 조회 api 구현

--- src/main/java/com/pickple/server/global/response/enums/SuccessCode.java
+++ src/main/java/com/pickple/server/global/response/enums/SuccessCode.java
@@ -25,6 +25,7 @@ public enum SuccessCode {
     NOTICE_LIST_GET_SUCCESS(20011, HttpStatus.OK, "공지사항 리스트 조회 성공"),
     MOIM_DESCRIPTION_GET_SUCCESS(20012, HttpStatus.OK, "모임에 해당하는 소개 조회 성공"),
     MOIM_QUESTION_LIST_GET_SUCCESS(20013, HttpStatus.OK, "모임 질문 목록 조회 성공"),
+    MOIM_BANNER_GET_SUCCESS(30014, HttpStatus.OK, "홈 배너 조회 성공"),
 
     //201 Created
     MOIM_CREATE_SUCCESS(20100, HttpStatus.CREATED, "모임 개설 성공");
