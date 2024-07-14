bb98aa60decdc8b67f1994f85865235512232acd
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-07-15 04:23:36 +0900

    hotfix : swagger cors 대응

--- src/main/java/com/pickple/server/global/config/WebConfig.java
+++ src/main/java/com/pickple/server/global/config/WebConfig.java
@@ -3,15 +3,11 @@ package com.pickple.server.global.config;
 import com.pickple.server.global.common.annotation.resolver.GuestIdResolver;
 import com.pickple.server.global.common.annotation.resolver.HostIdResolver;
 import com.pickple.server.global.common.annotation.resolver.UserIdResolver;
-import java.util.Arrays;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
@@ -22,19 +18,14 @@ public class WebConfig implements WebMvcConfigurer {
     private final GuestIdResolver guestIdResolver;
     private final HostIdResolver hostIdResolver;
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:8080");
-        config.addAllowedOrigin("http://localhost:8081");
-        config.addAllowedOrigin("http://localhost:5173");
-        config.addAllowedOrigin("https://pick-ple.com");
-        config.addAllowedHeader("*");
-        config.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:8081", "http://localhost:5173",
+                        "https://pick-ple.com:5173", "https://pick-ple.com")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(3000);
     }
 
     @Override
