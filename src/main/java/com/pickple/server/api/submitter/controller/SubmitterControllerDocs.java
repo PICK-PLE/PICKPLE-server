e8991a090a0607115e08e5576bd65bc7e55dd6f3
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-07-17 17:54:10 +0900

    docs : userId 파라미터 추가

--- src/main/java/com/pickple/server/api/submitter/controller/SubmitterControllerDocs.java
+++ src/main/java/com/pickple/server/api/submitter/controller/SubmitterControllerDocs.java
@@ -38,7 +38,10 @@ public interface SubmitterControllerDocs {
                     @ApiResponse(responseCode = "20024", description = "호스트 승인 신청 내역 조회 성공")
             }
     )
-    ApiResponseDto<List<SubmitterListGetResponse>> getSubmitterList();
+    ApiResponseDto<List<SubmitterListGetResponse>> getSubmitterList(
+            @Parameter(schema = @Schema(implementation = String.class), in = ParameterIn.PATH)
+            @UserId final Long userId
+    );
 
     @Operation(summary = "호스트 신청 승인")
     @ApiResponses(
