96308b8d0b58ae15bfbb413af1a086e4a4278430
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-08-11 03:47:19 +0900

    feat : requestDto에 RedirectUri 담도록 수정

--- src/main/java/com/pickple/server/global/auth/client/dto/UserLoginRequest.java
+++ src/main/java/com/pickple/server/global/auth/client/dto/UserLoginRequest.java
@@ -2,11 +2,15 @@ package com.pickple.server.global.auth.client.dto;
 
 import com.pickple.server.api.user.domain.SocialType;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 public record UserLoginRequest(
-        @NotNull(message = "소셜 로그인 종류가 입력되지 않았습니다.")
+        @NotBlank(message = "소셜 로그인 종류가 입력되지 않았습니다.")
         @Schema(description = "소셜로그인 타입", example = "KAKAO")
-        SocialType socialType
+        SocialType socialType,
+
+        @NotBlank
+        @Schema(description = "리다이텍트 uri 값", example = "https://pick-ple.com/kakao/redirection or http://localhost:5173/kakao/redirection")
+        String redirectUri
 ) {
 }
