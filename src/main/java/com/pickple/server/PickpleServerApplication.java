8d92d94d4d00812ddac654736c45729ed70b50b4
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-08-16 00:16:12 +0900

    feat : @EnableScheduling 추가

--- src/main/java/com/pickple/server/PickpleServerApplication.java
+++ src/main/java/com/pickple/server/PickpleServerApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFeignClients
 @SpringBootApplication
 @ImportAutoConfiguration(FeignAutoConfiguration.class)
 @EnableJpaAuditing
+@EnableScheduling
 public class PickpleServerApplication {
 
     public static void main(String[] args) {
