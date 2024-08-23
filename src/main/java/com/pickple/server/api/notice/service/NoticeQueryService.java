640aaf67c5f3a3645a125752057dd05e9ecc0763
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-08-23 23:59:33 +0900

    feat : service 구현

--- src/main/java/com/pickple/server/api/notice/service/NoticeQueryService.java
+++ src/main/java/com/pickple/server/api/notice/service/NoticeQueryService.java
@@ -3,6 +3,7 @@ package com.pickple.server.api.notice.service;
 import com.pickple.server.api.moim.domain.Moim;
 import com.pickple.server.api.moim.repository.MoimRepository;
 import com.pickple.server.api.notice.domain.Notice;
+import com.pickple.server.api.notice.dto.response.NoticeDetailGetResponse;
 import com.pickple.server.api.notice.dto.response.NoticeListGetByMoimResponse;
 import com.pickple.server.api.notice.repository.NoticeRepository;
 import com.pickple.server.global.util.DateTimeUtil;
@@ -36,5 +37,26 @@ public class NoticeQueryService {
                         .hostId(moim.getHost().getId())
                         .build())
                 .collect(Collectors.toList());
+    }
+
+    public NoticeDetailGetResponse getNoticeDetail(Long hostId, Long moimId, Long noticeId) {
+        Moim moim = moimRepository.findMoimByIdOrThrow(moimId);
+        Notice notice = noticeRepository.findNoticeByIdOrThrow(noticeId);
+
+        return NoticeDetailGetResponse.builder()
+                .hostImageUrl(moim.getHost().getImageUrl())
+                .hostNickname(moim.getHost().getNickname())
+                .title(notice.getTitle())
+                .content(notice.getContent())
+                .dateTime(notice.getCreatedAt())
+                .commentNumber(0)
+                .isPrivate(notice.isPrivate())
+                .isOwner(checkOwner(hostId, moimId))
+                .build();
+    }
+
+    public boolean checkOwner(Long hostId, Long moimId) {
+        Moim moim = moimRepository.findMoimByIdOrThrow(moimId);
+        return hostId.equals(moim.getHost().getId());
     }
 }
