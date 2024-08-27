509b6c20d7bf9020d7806940bab469f951c8afd4
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-08-27 17:36:30 +0900

    feat : 댓글 엔티티 구현

---
+++ src/main/java/com/pickple/server/api/comment/domain/Comment.java
@@ -0,0 +1,45 @@
+package com.pickple.server.api.comment.domain;
+
+import com.pickple.server.api.notice.domain.Notice;
+import com.pickple.server.api.user.domain.User;
+import com.pickple.server.global.common.domain.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "comments")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "notice_id")
+    private Notice notice;
+
+    private String commenterImageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User commenter;
+
+    private String commentContent;
+
+    private boolean isOwner;
+}
