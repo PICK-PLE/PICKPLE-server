d292d7c6053f8f7218a5abddcefa2e71d6576052
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-08-27 17:36:17 +0900

    feat : notice와 comment 매핑

--- src/main/java/com/pickple/server/api/notice/domain/Notice.java
+++ src/main/java/com/pickple/server/api/notice/domain/Notice.java
@@ -1,7 +1,9 @@
 package com.pickple.server.api.notice.domain;
 
+import com.pickple.server.api.comment.domain.Comment;
 import com.pickple.server.api.moim.domain.Moim;
 import com.pickple.server.global.common.domain.BaseTimeEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -9,8 +11,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -40,4 +45,7 @@ public class Notice extends BaseTimeEntity {
 
     @Size(max = 500)
     private String imageUrl;
+
+    @OneToMany(mappedBy = "notice", cascade = CascadeType.REMOVE)
+    private List<Comment> comments = new ArrayList<>();
 }
