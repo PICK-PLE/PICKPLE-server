34f2fd903db35d38c86d6f8f26c797b07a001ce1
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-08-27 22:28:15 +0900

    [feat] 참가자 수 counter 구현

--- src/main/java/com/pickple/server/api/moimsubmission/repository/MoimSubmissionRepository.java
+++ src/main/java/com/pickple/server/api/moimsubmission/repository/MoimSubmissionRepository.java
@@ -28,4 +28,7 @@ public interface MoimSubmissionRepository extends JpaRepository<MoimSubmission, 
 
     @Query(value = "SELECT COUNT(*) FROM moim_submissions WHERE moim_id = :moimId AND moim_submission_state = 'approved'", nativeQuery = true)
     long countApprovedMoimSubmissions(@Param("moimId") Long moimId);
+
+    @Query("SELECT COUNT(ms) FROM MoimSubmission ms WHERE ms.moim.id IN :moimIds AND ms.moimSubmissionState = 'approved'")
+    int countApprovedSubmissionsByMoimIds(@Param("moimIds") List<Long> moimIds);
 }
