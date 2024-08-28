03cf714a6f32ce97e7f61477614c0a06aef7a718
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-08-29 01:55:36 +0900

    [chore] 스웨거 반영

--- src/main/java/com/pickple/server/global/common/external/s3/S3ControllerDocs.java
+++ src/main/java/com/pickple/server/global/common/external/s3/S3ControllerDocs.java
@@ -1,33 +1,26 @@
 package com.pickple.server.global.common.external.s3;
 
+import com.pickple.server.global.common.external.s3.dto.PreSignedUrlClientRequest;
+import com.pickple.server.global.common.external.s3.dto.PreSignedUrlResponse;
 import com.pickple.server.global.response.ApiResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "Image - PreSigned Url", description = "이미지 업로드 할 Url 받기")
 public interface S3ControllerDocs {
 
-    @Operation(summary = "모임 이미지 업로드 presigned url")
+    @Operation(summary = "이미지 업로드 presigned url")
     @ApiResponses(
             value = {
-                    @ApiResponse(responseCode = "20008", description = "모임 presigned url 발급 성공"),
+                    @ApiResponse(responseCode = "20009", description = "presigned url 발급 성공"),
                     @ApiResponse(responseCode = "50001", description = "S3 presigned url을 받아오기에 실패했습니다.")
             }
     )
-    ApiResponseDto<List<PreSignedUrlResponse>> getMoimPreSignedUrl(@PathVariable int count);
-
-    @Operation(summary = "공지 이미지 업로드 presigned url")
-    @ApiResponses(
-            value = {
-                    @ApiResponse(responseCode = "20008", description = "공지 presigned url 발급 성공"),
-                    @ApiResponse(responseCode = "50001", description = "S3 presigned url을 받아오기에 실패했습니다.")
-            }
-    )
-    ApiResponseDto<List<PreSignedUrlResponse>> getNoticePreSignedUrl(@PathVariable int count);
+    ApiResponseDto<List<PreSignedUrlResponse>> getPreSignedUrl(@RequestBody PreSignedUrlClientRequest clientRequest);
 }
 
 
