8ae43c2d1aa45253c76bda6bf09a12acf0db75ba
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-08-29 01:55:16 +0900

    [refactor] presiginedUrl 요청 방식 리팩토링

--- src/main/java/com/pickple/server/global/common/external/s3/S3Controller.java
+++ src/main/java/com/pickple/server/global/common/external/s3/S3Controller.java
@@ -1,11 +1,13 @@
 package com.pickple.server.global.common.external.s3;
 
+import com.pickple.server.global.common.external.s3.dto.PreSignedUrlClientRequest;
+import com.pickple.server.global.common.external.s3.dto.PreSignedUrlResponse;
 import com.pickple.server.global.response.ApiResponseDto;
 import com.pickple.server.global.response.enums.SuccessCode;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,22 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class S3Controller implements S3ControllerDocs {
     private final S3Service s3Service;
 
-    @GetMapping("/v1/moim-image-list/upload/{count}")
+    @GetMapping("/v2/image/upload")
     @Override
-    public ApiResponseDto<List<PreSignedUrlResponse>> getMoimPreSignedUrl(
-            @PathVariable int count
+    public ApiResponseDto<List<PreSignedUrlResponse>> getPreSignedUrl(
+            @RequestBody PreSignedUrlClientRequest request
     ) {
         return ApiResponseDto.success(SuccessCode.PRESIGNED_URL_GET_SUCCESS,
-                s3Service.getUploadPreSignedUrlList(S3BucketDirectory.MOIM_PREFIX, count));
-    }
-
-    @GetMapping("/v1/notice-image-list/upload/{count}")
-    @Override
-    public ApiResponseDto<List<PreSignedUrlResponse>> getNoticePreSignedUrl(
-            @PathVariable int count
-    ) {
-        return ApiResponseDto.success(SuccessCode.PRESIGNED_URL_GET_SUCCESS,
-                s3Service.getUploadPreSignedUrlList(S3BucketDirectory.NOTICE_PREFIX, count));
+                s3Service.getUploadPreSignedUrlList(request));
     }
 }
 
