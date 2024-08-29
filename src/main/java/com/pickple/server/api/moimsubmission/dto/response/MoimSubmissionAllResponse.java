4ad4921365397f222de5618a46aaa4f4ed48733b
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-08-30 01:47:17 +0900

    [feat] 코드리뷰 반영 - id값 Long으로 통일

--- src/main/java/com/pickple/server/api/moimsubmission/dto/response/MoimSubmissionAllResponse.java
+++ src/main/java/com/pickple/server/api/moimsubmission/dto/response/MoimSubmissionAllResponse.java
@@ -6,13 +6,13 @@ import lombok.Builder;
 
 @Builder
 public record MoimSubmissionAllResponse(
-        long moimSubmissionId,       //신청 순서를 위한 모임신청id
+        Long moimSubmissionId,       //신청 순서를 위한 모임신청id
         String date,                 //신청날짜
         String moimSubmissionState,  //모임 신청상태
-        long guestId,                //게스트 id
+        Long guestId,                //게스트 id
         String guestNickname,        //게스트 닉네임
         String kakaoNickname,        //카카오에서 받아오는 닉네임
-        long moimId,                 //모임 id
+        Long moimId,                 //모임 id
         String moimTitle,            //모임명
         String hostNickname,         //스픽커명
         QuestionInfo questionList,   //질문 리스트
