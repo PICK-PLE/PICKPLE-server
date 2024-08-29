2cf6795675f23e67a4d1230c654789449d508838
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-08-30 03:04:40 +0900

    docs : swagger 명세서 추가

--- src/main/java/com/pickple/server/api/comment/controller/CommentControllerDocs.java
+++ src/main/java/com/pickple/server/api/comment/controller/CommentControllerDocs.java
@@ -1,6 +1,7 @@
 package com.pickple.server.api.comment.controller;
 
 import com.pickple.server.api.comment.dto.request.CommentCreateRequest;
+import com.pickple.server.api.comment.dto.response.CommentGetResponse;
 import com.pickple.server.global.common.annotation.UserId;
 import com.pickple.server.global.response.ApiResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,6 +11,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -30,5 +32,16 @@ public interface CommentControllerDocs {
             @UserId Long userId,
             @PathVariable Long noticeId,
             @RequestBody CommentCreateRequest commentCreateRequest
+    );
+
+    @Operation(summary = "공지사항 댓글 전체 조회")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "20032", description = "공지사항 댓글 작성 성공"),
+                    @ApiResponse(responseCode = "40409", description = "존재하지 않는 공지사항입니다.")
+            }
+    )
+    ApiResponseDto<List<CommentGetResponse>> getCommentListByNotice(
+            @PathVariable Long noticeId
     );
 }
