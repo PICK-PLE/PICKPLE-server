ec97546a593c9ad5114a2ebb4b9e3fd7d7bb75b9
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-08-30 01:46:56 +0900

    feat : controller 구현

--- src/main/java/com/pickple/server/api/comment/controller/CommentController.java
+++ src/main/java/com/pickple/server/api/comment/controller/CommentController.java
@@ -1,12 +1,16 @@
 package com.pickple.server.api.comment.controller;
 
 import com.pickple.server.api.comment.dto.request.CommentCreateRequest;
+import com.pickple.server.api.comment.dto.response.CommentGetResponse;
 import com.pickple.server.api.comment.service.CommentCommandService;
+import com.pickple.server.api.comment.service.CommentQueryService;
 import com.pickple.server.global.common.annotation.UserId;
 import com.pickple.server.global.response.ApiResponseDto;
 import com.pickple.server.global.response.enums.SuccessCode;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommentController implements CommentControllerDocs {
 
     private final CommentCommandService commentCommandService;
+    private final CommentQueryService commentQueryService;
 
     @PostMapping("/v2/notice/{noticeId}/comment")
     public ApiResponseDto createComment(@UserId Long userId,
@@ -26,5 +31,11 @@ public class CommentController implements CommentControllerDocs {
                                         @Valid @RequestBody CommentCreateRequest commentCreateRequest) {
         commentCommandService.createComment(userId, noticeId, commentCreateRequest);
         return ApiResponseDto.success(SuccessCode.COMMENT_POST_SUCCESS);
+    }
+
+    @GetMapping("/v2/notice/{noticeId}/comment-list")
+    public ApiResponseDto<List<CommentGetResponse>> getCommentByNotice(@PathVariable Long noticeId) {
+        return ApiResponseDto.success(SuccessCode.COMMENT_LIST_BY_NOTICE_GET_SUCCESS,
+                commentQueryService.getCommentListByNotice(noticeId));
     }
 }
