e561039eb0b5f32d1d2411dd6b45929dfa3b9f1a
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-08-30 04:05:38 +0900

    feat : 성공 응답 코드 추가

--- src/main/java/com/pickple/server/global/response/enums/SuccessCode.java
+++ src/main/java/com/pickple/server/global/response/enums/SuccessCode.java
@@ -42,7 +42,8 @@ public enum SuccessCode {
     MOIM_SUBMISSION_STATE_UPDATE_SUCCESS(20030, HttpStatus.OK, "모임 신청 내역 승인대기로 변경 성공"),
     NOTICE_DETAIL_GET_SUCCESS(20031, HttpStatus.OK, "공지사항 상세 조회 성공"),
     MOIM_SUBMISSION_ALL_GET_SUCCESS(20033, HttpStatus.OK, "모임 참여 신청 전체 조회 성공"),
- 
+    GUEST_PROFILE_UPDATE_SUCCESS(20034, HttpStatus.OK, "게스트 프로필 수정 성공"),
+
     // 201 Created
     MOIM_CREATE_SUCCESS(20100, HttpStatus.CREATED, "모임 개설 성공"),
     REVIEW_CREATE_SUCCESS(20101, HttpStatus.CREATED, "리뷰 생성 성공");
