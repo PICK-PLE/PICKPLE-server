8f775f1d6fb38da95129660b2997fe1a7882cbbb
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-09-05 22:27:35 +0900

    feat : dto 구현

---
+++ src/main/java/com/pickple/server/api/moim/dto/response/MoimGetResponse.java
@@ -0,0 +1,14 @@
+package com.pickple.server.api.moim.dto.response;
+
+import com.pickple.server.api.moim.domain.DateInfo;
+import lombok.Builder;
+
+@Builder
+public record MoimGetResponse(
+        String title,
+
+        String hostNickname,
+
+        DateInfo dateList
+) {
+}
