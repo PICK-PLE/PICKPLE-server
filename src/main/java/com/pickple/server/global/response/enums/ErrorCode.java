2d15a346c1c790b2eaf16f6f8e60f7a3a3f83923
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-09-25 00:32:24 +0900

    feat : access token 만료시 에러처리

--- src/main/java/com/pickple/server/global/response/enums/ErrorCode.java
+++ src/main/java/com/pickple/server/global/response/enums/ErrorCode.java
@@ -23,11 +23,11 @@ public enum ErrorCode {
     NO_SUBMISSION_FOUND_FOR_REVIEW(40011, HttpStatus.BAD_REQUEST, "리뷰를 작성할 수 있는 신청이 없습니다"),
     NOT_AUTHOR(40012, HttpStatus.BAD_REQUEST, "해당 댓글의 작성자가 아닙니다."),
     DUPLICATION_APPROVE_SUBMITTER(40013, HttpStatus.BAD_REQUEST, "이미 호스트입니다."),
-    
+
     // 401 Unauthorized
     ACCESS_TOKEN_EXPIRED(40100, HttpStatus.UNAUTHORIZED, "액세스 토큰이 만료되었습니다."),    //code 건들지 말것, 클라 분기처리함
     TOKEN_INCORRECT_ERROR(40101, HttpStatus.UNAUTHORIZED, "리프레시 토큰이 유효하지 않습니다."),    //code 건들지 말것, 클라 분기처리함
-    EMPTY_PRINCIPAL(40103, HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+    EMPTY_PRINCIPAL(40102, HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
 
     //403 Forbidden
     NOT_ADMIN(40301, HttpStatus.UNAUTHORIZED, "관리자 계정이 아닙니다."),
