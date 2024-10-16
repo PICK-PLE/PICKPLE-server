0754fbf2fc82387a87cc70680aac4740b79b8c41
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-10-17 00:18:40 +0900

    feat : ReviewRepository default 메소드 구현

--- src/main/java/com/pickple/server/api/review/repository/ReviewRepository.java
+++ src/main/java/com/pickple/server/api/review/repository/ReviewRepository.java
@@ -1,10 +1,20 @@
 package com.pickple.server.api.review.repository;
 
 import com.pickple.server.api.review.domain.Review;
+import com.pickple.server.global.exception.CustomException;
+import com.pickple.server.global.response.enums.ErrorCode;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Optional<Review> findReviewById(Long id);
+
+    default Review findReviewByIdOrThrow(Long id) {
+        return findReviewById(id)
+                .orElseThrow(() -> new CustomException(ErrorCode.REVIEW_NOT_FOUND));
+    }
 
     boolean existsByMoimIdAndGuestId(Long moimId, Long guestId);
 
